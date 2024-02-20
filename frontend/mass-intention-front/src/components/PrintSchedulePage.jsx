20fe38ca47bba6a11ed4ef97c39993819878d9b5
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-02-20 18:23:04 +0800

    feat: added printing of schedules

--- frontend/mass-intention-front/src/components/PrintSchedulePage.jsx
+++ frontend/mass-intention-front/src/components/PrintSchedulePage.jsx
@@ -1,31 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
+import * as XLSX from "xlsx";
 import LoadingOverlay from "./LoadingOverlay";
-import axios from 'axios'
 
 const PrintSchedulePage = () => {
-  const serverURL = "http://localhost:8080"
+  const [loading, setLoading] = useState(false);
+  const serverURL = "http://localhost:8080";
+
+  const convertDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleString();
+  };
+
+  const printData = (data) => {
+    if (data.length === 0) {
+      return alert("No schedules found!");
+    } else {
+      // Remove _id and __v keys from each object
+      const formattedData = data.map((item) => {
+        delete item._id;
+        delete item.__v;
+        item.date = convertDate(item.date);
+        return item;
+      });
+
+      // Arrange by date oldest first
+      formattedData.sort((a, b) => {
+        return new Date(a.date) - new Date(b.date);
+      });
+
+      // Convert data to Excel format
+      const worksheet = XLSX.utils.json_to_sheet(formattedData);
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, "Schedule Data");
+
+      // Save Excel file
+      const excelBuffer = XLSX.write(workbook, {
+        bookType: "xlsx",
+        type: "array",
+      });
+      const excelBlob = new Blob([excelBuffer], {
+        type: "excel",
+      });
+      saveAs(excelBlob, "schedule_data.xlsx");
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = Object.fromEntries(new FormData(e.target).entries());
-    console.log(formData);
+    setLoading(true);
 
-
-    axios.get(`${serverURL}/api/printFromDateRange`, {
+    axios
+      .get(`${serverURL}/api/printFromDateRange`, {
         params: {
           startDate: new Date(formData.startDate),
-          endDate: new Date(formData.endDate)
-        }
+          endDate: new Date(formData.endDate),
+        },
       })
-      .then(response => {
+      .then((response) => {
         // Handle the response data
         console.log(response.data);
+        printData(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         // Handle errors
-        console.error('Error fetching data:', error);
-      });
+        console.error("Error fetching data:", error);
+      })
+      .finally(()=>setLoading(false));
   };
+
   return (
     <>
       <div className="mt-4 mx-3 px-2 lg:px-32">
@@ -75,7 +120,7 @@ const PrintSchedulePage = () => {
           </button>
         </form>
       </div>
-
+      {loading && <LoadingOverlay />}
     </>
   );
 };
